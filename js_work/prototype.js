f5f66f6192c0338b603ce0f087fd958975af9482
pallab-BJIT <pallab.majumdar@bjitacademy.com>
2023-08-08 15:58:13 +0600

    updated the task of week 2 day 2

--- js_work/prototype.js
+++ js_work/prototype.js
@@ -3,14 +3,14 @@ function Person(name, age) {
   this.age = age;
 }
 
-Person.prototype.sayHello = function (gender) {
-  console.log(
-    `My name is ${this.name} and my age is ${this.age} and my gender is ${gender}`
-  );
-};
-
 const person1 = new Person("John Doe", 28);
 const person2 = new Person("Alice Smith", 36);
 
-person1.sayHello("male");
-person2.sayHello("female");
+person1.sayHello = function () {
+  console.log(`My name is ${this.name} and my age is ${this.age}`);
+};
+
+person1.sayHello();
+person2.gender = "female";
+
+console.log(person2.gender);
