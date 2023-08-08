51b5e0797883460b7c7713f3622ddb796584dbcf
pallab-BJIT <pallab.majumdar@bjitacademy.com>
2023-08-08 16:48:40 +0600

    finshed today's task

--- js_work/reduce.js
+++ js_work/reduce.js
@@ -1,10 +1,7 @@
 const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const ans = num.reduce((initialValue, currentValue) => {
-  if (currentValue % 2 == 0) {
-    return initialValue + currentValue;
-  }
-  return initialValue;
+  return currentValue % 2 == 0 ? initialValue + currentValue : initialValue;
 }, 0);
 
 console.log(ans);
